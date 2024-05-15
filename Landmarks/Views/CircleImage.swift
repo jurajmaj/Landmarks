7566a116b4cce42f6e79c56f2b88f5dd33f9ce52
Juraj Maják <juraj.majak@student.tuke.sk>
2024-05-15 12:40:15 +0200

    Initial commit

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
